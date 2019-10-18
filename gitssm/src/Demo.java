a09ac801a3fdf95f2a801a9d2cdc547484b40d3c
liye-upup <1184668038@qq.com>
2019-10-18 14:52:51 +0800

    Initial commit

---
+++ gitssm/src/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @author李烨
+ * @site www.yilufa.com
+ * @company xxx公司
+ * @create  2019-10-18 14:42
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+}
